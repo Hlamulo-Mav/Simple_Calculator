457df119bac52cd8f577c64d757d732ba0924442
nothembah <74840172+nothembah@users.noreply.github.com>
2021-06-15 17:37:37 +0200

    Update index.js
    Added multiply function

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ function subtract(num1, num2) {
     return minus;
 }
 
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
 let x = 7;
 let y = 4;
 
 console.log(add(x, y));
 
 console.log(subtract(x, y));
+
+console.log(multiply(x, y));
